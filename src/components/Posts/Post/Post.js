1972255b5296807a3eadbc99c73846b535c6e798
Udokailodigwe <ilodigweudoka@gmail.com>
2022-06-14 18:36:25 +0300

    Created post folder to represent single posts

---
+++ src/components/Posts/Post/Post.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { } from '@material-ui/core';
+
+import useStyles from './styles';
+
+const Post = () => {
+    const classes = useStyles();
+    return (
+        <h1>POST</h1>
+    )
+}
+
+export default Post;
