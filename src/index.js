73dabe2f0bc8a66c04c441e7a16ede120d115977
Udokailodigwe <ilodigweudoka@gmail.com>
2022-06-14 18:24:40 +0300

    Imported redux and its packages, as well store for Provider component

--- src/index.js
+++ src/index.js
@@ -1,17 +1,16 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
+import ReactDom from 'react-dom/client';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware, compose } from 'redux';
+import thunk from 'redux-thunk';
+
+import reducers from './reducers';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const store = createStore(reducers, compose(applyMiddleware(thunk)))
+ReactDom.createRoot(document.getElementById('root'))
+    .render(
+        <Provider store={store}>
+            <App />
+        </Provider>,
+    );
