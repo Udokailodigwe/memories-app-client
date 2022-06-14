149cb5c396bb5ebc078bdc85f5c8109089c345d6
Udokailodigwe <ilodigweudoka@gmail.com>
2022-06-14 18:28:08 +0300

    Created action folder for action function and implemented disptach action enabled by redux-thunk

---
+++ src/actions/posts.js
@@ -0,0 +1,19 @@
+import * as api from '../api';
+
+/*creating actions with dispatch from redux-thunk*/
+export const getPosts = () => async (dispatch) => {
+    try {
+        const { data } = await api.fetchPosts();
+        dispatch({ type: 'FETCH_ALL', payload: data })
+    } catch (error) {
+        console.log(error.message);
+    }
+}
+export const createPosts = (post) => async (dispatch) => {
+    try {
+        const { data } = await api.createPosts(post);
+        dispatch({ type: 'CREATE', payload: data })
+    } catch (error) {
+        console.log(error);
+    }
+}
