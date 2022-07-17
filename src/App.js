b304836340a2dd42a75a5aa44847ab9b7a7d0c4f
Udokailodigwe <ilodigweudoka@gmail.com>
2022-07-18 00:33:28 +0300

    styled component

--- src/App.js
+++ src/App.js
@@ -23,7 +23,7 @@ const App = () => {
 
     return (
         <Container maxWidth="lg">
-            <AppBar className={classes.appBar} position="static" color="secondary">
+            <AppBar className={classes.appBar} position="static" >
                 <Typography className={classes.heading} variant="h2" align="center">Pet Memories</Typography>
                 <img className={classes.image} src={bannerImage} alt="petImage" height="60" />
             </AppBar>
